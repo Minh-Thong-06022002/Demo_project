e74725d4c4e00cfa4ec2227853c79f210b77f325
Minh-Thong-06022002 <nguyenminhthong11b@gmail.com>
2023-08-13 23:32:48 +0700

    create project

---
+++ src/App.vue
@@ -0,0 +1,36 @@
+<template>
+  <ul></ul>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      storedResources: [
+        {
+          id: 'offical guide',
+          title: 'Offical Guide',
+          description: 'The Official Vus JS Documentation',
+          link: 'http://vuejs.org',
+        },
+        {
+          id: 'Google',
+          title: 'Google Chrome',
+          description: 'Learn to Google',
+          link: 'http://google.org',
+        }
+      ], 
+    }
+  },
+}
+</script>
+
+<style>
+html {
+  font-family: sans-serif;
+}
+
+body {
+  margin: 0;
+}
+</style>
